89cba09388d4451c2408d09b77ebd3e12b9b2e0b
JosJuice <josjuice@gmail.com>
2014-05-27 14:16:36 +0200

    Remove unneeded code from index.php

--- upload/index.php
+++ upload/index.php
@@ -38,10 +38,6 @@ $page_title = pun_htmlspecialchars($pun_config['o_board_title']);
 define('PUN_ALLOW_INDEX', 1);
 require PUN_ROOT.'header.php';
 
-// Fetch all new topics since user's last visit
-if (!$pun_user['is_guest'])
-	$new_topics = get_all_new_topics();
-
 // Print the categories and forums
 $result = $db->query('SELECT c.id AS cid, c.cat_name, f.id AS fid, f.forum_name, f.forum_desc, f.redirect_url, f.moderators, f.num_topics, f.num_posts, f.last_post, f.last_post_id, f.last_poster FROM '.$db->prefix.'categories AS c INNER JOIN '.$db->prefix.'forums AS f ON c.id=f.cat_id LEFT JOIN '.$db->prefix.'forum_perms AS fp ON (fp.forum_id=f.id AND fp.group_id='.$pun_user['g_id'].') WHERE fp.read_forum IS NULL OR fp.read_forum=1 ORDER BY c.disp_position, c.id, f.disp_position', true) or error('Unable to fetch category/forum list', __FILE__, __LINE__, $db->error());
 
@@ -81,14 +77,6 @@ while ($cur_forum = $db->fetch_assoc($result))
 	$item_status = '';
 	$icon_text = $lang_common['Normal icon'];
 	$icon_type = 'icon';
-
-	// Are there new posts?
-	if (!$pun_user['is_guest'] && forum_is_new($cur_forum['fid'], $cur_forum['last_post']))
-	{
-		$item_status = 'inew';
-		$icon_text = $lang_common['New icon'];
-		$icon_type = 'icon inew';
-	}
 
 	// Is this a redirect forum?
 	if ($cur_forum['redirect_url'] != '')
