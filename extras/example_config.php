3ed924103ddc06c71c45b98228b4efab962b25bb
JosJuice <josjuice@gmail.com>
2014-05-28 20:55:32 +0200

    Add example_config.php

---
+++ extras/example_config.php
@@ -0,0 +1,19 @@
+<?php
+
+// This is an example config. Modify it so that it applies to your database.
+
+// $db_type can be set to one of the following:
+// 'mysql'	(MySQL)
+// 'mysqli'	(MySQL improved. You should use this instead of 'mysql')
+// 'pgsql'	(PostgreSQL)
+// 'sqlite'	(SQLite)
+
+$db_type = 'blabla';
+$db_host = 'blabla';
+$db_name = 'blabla';
+$db_username = 'blabla';
+$db_password = 'blabla';
+$db_prefix = '';
+$p_connect = false;
+
+define('PUN', 1);
